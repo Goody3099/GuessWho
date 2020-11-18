000b8a833f1f9d21ce42f1fb64b7130bf712a0ef
Matthew Goodman <mattgoodman3099@gmail.com>
2020-11-18 12:02:34 -0600

    player side works, still need to build computer side for the back and forth

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace GuessWho
 {
@@ -25,7 +27,7 @@ namespace GuessWho
                 hat = true,
                 glasses = false,
                 male = true,
-                hairColor = "Blond",
+                hairColor = "Blonde",
                 facialHair = true,
                 eyeColor = "Blue",
                 earrings = false,
@@ -69,7 +71,7 @@ namespace GuessWho
                 hat = true,
                 glasses = false,
                 male = true,
-                hairColor = "Blond",
+                hairColor = "Blonde",
                 facialHair = false,
                 eyeColor = "Brown",
                 earrings = false,
@@ -124,7 +126,7 @@ namespace GuessWho
                 hat = false,
                 glasses = true,
                 male = true,
-                hairColor = "Blond",
+                hairColor = "Blonde",
                 facialHair = true,
                 eyeColor = "Blue",
                 earrings = false,
@@ -146,7 +148,7 @@ namespace GuessWho
                 hat = true,
                 glasses = false,
                 male = false,
-                hairColor = "Blond",
+                hairColor = "Blonde",
                 facialHair = false,
                 eyeColor = "Green",
                 earrings = true,
@@ -212,7 +214,7 @@ namespace GuessWho
                 hat = false,
                 glasses = true,
                 male = false,
-                hairColor = "Blond",
+                hairColor = "Blonde",
                 facialHair = false,
                 eyeColor = "Green",
                 earrings = true,
@@ -267,7 +269,7 @@ namespace GuessWho
                 hat = false,
                 glasses = false,
                 male = false,
-                hairColor = "Blond",
+                hairColor = "Blonde",
                 facialHair = false,
                 eyeColor = "Brown",
                 earrings = true,
@@ -298,30 +300,265 @@ namespace GuessWho
             listOfCharacters.Add(Evelyn);
             listOfCharacters.Add(Abigail);
             listOfCharacters.Add(Emily);
-            foreach (var character in listOfCharacters)
+            List<Characters> playerBoard = listOfCharacters;
+            List<Characters> computerBoard = listOfCharacters;
+            Characters pc = listOfCharacters[randomNumber()];
+            Characters cc = listOfCharacters[randomNumber()];
+
+            while (true)
             {
-                Console.WriteLine($@"
-                Name: {character.name}
-                Male: {character.male}
-                Hat: {character.hat}
-                Glasses: {character.glasses}
-                Earrings: {character.earrings}
-                Facial Hair: {character.facialHair}
-                Hair Color: {character.hairColor}
-                Eye Color: {character.eyeColor}
+                displayPlayerBoard(playerBoard);
+                questions();
+                string response = Console.ReadLine();
+                playerBoard = evalResponse(response, cc, playerBoard);
+                if (playerBoard.Count() == 1)
+                {
+                    Console.WriteLine("You Won!!!");
+                    break;
+                }
+                playerCharacter(pc);
+            }
+        }
+        static void displayPlayerBoard(List<Characters> x)
+        {
+            foreach (var pc in x)
+            {
+                Console.WriteLine($@" {pc.name}
+                male: {pc.male}
+                Hat: {pc.hat}
+                Glasses: {pc.glasses}
+                Earrings: {pc.earrings}
+                Facial Hair: {pc.facialHair}
+                Hair Color: {pc.hairColor}
+                Eye Color: {pc.eyeColor}
                 ");
             }
         }
-        public class Characters
+        static void playerCharacter(Characters pc)
         {
-            public string name;
-            public bool hat;
-            public bool glasses;
-            public bool male;
-            public string hairColor;
-            public bool facialHair;
-            public string eyeColor;
-            public bool earrings;
+            Console.WriteLine($@"Player's Character: {pc.name}
+                male: {pc.male}
+                Hat: {pc.hat}
+                Glasses: {pc.glasses}
+                Earrings: {pc.earrings}
+                Facial Hair: {pc.facialHair}
+                Hair Color: {pc.hairColor}
+                Eye Color: {pc.eyeColor}
+                ");
+        }
+        static int randomNumber()
+        {
+            Random r = new Random();
+            int random = r.Next(0, 24);
+            return random;
+        }
+        static void questions()
+        {
+            Console.WriteLine(@"Ask a question.
+            1). Is your character male?
+            2). Does your character have glasses?
+            3). Does your character have a hat?
+            4). Does your character have Facial hair?
+            5). Does your character have earrings?
+            6). Does your character have blue eyes?
+            7). Does your character have brown eyes?
+            8). Does your character have green eyes?
+            9). Does your character have red hair?
+            10). Does your character have blonde hair?
+            11). Does your character have black hair?
+            12). Does your character have brown hair?
+            13). Guess Character!");
+        }
+        static List<Characters> evalResponse(string evalResponse, Characters cc, List<Characters> playerBoard)
+        {
+            if (evalResponse == "1")
+            {
+                if (cc.male == true)
+                {
+                    Console.WriteLine("Yes, My character is male.");
+                    return playerBoard.Where(e => e.male == true).ToList();
+                }
+                else
+                {
+                    Console.WriteLine("No, My character is not male.");
+                    return playerBoard.Where(e => e.male != true).ToList();
+                }
+            }
+            else if (evalResponse == "2")
+            {
+                if (cc.glasses == true)
+                {
+                    Console.WriteLine("Yes, My character has glasses.");
+                    return playerBoard.Where(e => e.glasses == true).ToList();
+                }
+                else
+                {
+                    Console.WriteLine("No, My character does not have glasses.");
+                    return playerBoard.Where(e => e.glasses != true).ToList();
+
+                }
+            }
+            else if (evalResponse == "3")
+            {
+                if (cc.hat == true)
+                {
+                    Console.WriteLine("Yes, My character has a hat.");
+                    return playerBoard.Where(e => e.hat == true).ToList();
+                }
+                else
+                {
+                    Console.WriteLine("No, My character does not have a hat.");
+                    return playerBoard.Where(e => e.hat != true).ToList();
+
+                }
+            }
+            else if (evalResponse == "4")
+            {
+                if (cc.facialHair == true)
+                {
+                    Console.WriteLine("Yes, My character has facial hair.");
+                    return playerBoard.Where(e => e.facialHair == true).ToList();
+                }
+                else
+                {
+                    Console.WriteLine("No, My character does not have facial hair.");
+                    return playerBoard.Where(e => e.facialHair != true).ToList();
+
+                }
+            }
+            else if (evalResponse == "5")
+            {
+                if (cc.earrings == true)
+                {
+                    Console.WriteLine("Yes, My character has earrings.");
+                    return playerBoard.Where(e => e.earrings == true).ToList();
+                }
+                else
+                {
+                    Console.WriteLine("No, My character does not have earrings.");
+                    return playerBoard.Where(e => e.earrings != true).ToList();
+
+                }
+            }
+            else if (evalResponse == "6")
+            {
+                if (cc.eyeColor == "Blue")
+                {
+                    Console.WriteLine("Yes, My character has blue eyes.");
+                    return playerBoard.Where(e => e.eyeColor == "Blue").ToList();
+                }
+                else
+                {
+                    Console.WriteLine("No, My character does not have blue eyes.");
+                    return playerBoard.Where(e => e.eyeColor != "Blue").ToList();
+
+                }
+            }
+            else if (evalResponse == "7")
+            {
+                if (cc.eyeColor == "Brown")
+                {
+                    Console.WriteLine("Yes, My character has brown eyes.");
+                    return playerBoard.Where(e => e.eyeColor == "Brown").ToList();
+                }
+                else
+                {
+                    Console.WriteLine("No, My character does not have brown eyes.");
+                    return playerBoard.Where(e => e.eyeColor != "Brown").ToList();
+
+                }
+            }
+            else if (evalResponse == "8")
+            {
+                if (cc.eyeColor == "Green")
+                {
+                    Console.WriteLine("Yes, My character has green eyes.");
+                    return playerBoard.Where(e => e.eyeColor == "Green").ToList();
+                }
+                else
+                {
+                    Console.WriteLine("No, My character does not have green eyes.");
+                    return playerBoard.Where(e => e.eyeColor != "Green").ToList();
+
+                }
+            }
+            else if (evalResponse == "9")
+            {
+                if (cc.hairColor == "Red")
+                {
+                    Console.WriteLine("Yes, My character has red hair.");
+                    return playerBoard.Where(e => e.hairColor == "Red").ToList();
+                }
+                else
+                {
+                    Console.WriteLine("No, My character does not have red hair.");
+                    return playerBoard.Where(e => e.hairColor != "Red").ToList();
+
+                }
+            }
+            else if (evalResponse == "10")
+            {
+                if (cc.hairColor == "Blonde")
+                {
+                    Console.WriteLine("Yes, My character has blonde hair.");
+                    return playerBoard.Where(e => e.hairColor == "Blonde").ToList();
+                }
+                else
+                {
+                    Console.WriteLine("No, My character does not have blonde hair.");
+                    return playerBoard.Where(e => e.hairColor != "Blonde").ToList();
+
+                }
+            }
+            else if (evalResponse == "11")
+            {
+                if (cc.hairColor == "Black")
+                {
+                    Console.WriteLine("Yes, My character has black hair.");
+                    return playerBoard.Where(e => e.hairColor == "Black").ToList();
+                }
+                else
+                {
+                    Console.WriteLine("No, My character does not have black hair.");
+                    return playerBoard.Where(e => e.hairColor != "Black").ToList();
+
+                }
+            }
+            else if (evalResponse == "12")
+            {
+                if (cc.hairColor == "Brown")
+                {
+                    Console.WriteLine("Yes, My character has brown hair.");
+                    return playerBoard.Where(e => e.hairColor == "Brown").ToList();
+                }
+                else
+                {
+                    Console.WriteLine("No, My character does not have brown hair.");
+                    return playerBoard.Where(e => e.hairColor != "Brown").ToList();
+
+                }
+            }
+            else if (evalResponse == "13")
+            {
+                Console.WriteLine("Who do you guess?");
+                string x = Console.ReadLine();
+                if (cc.name.ToLower() == x.ToLower())
+                {
+                    Console.WriteLine($"Yes, My character is {x}!");
+                    return playerBoard.Where(e => e.name.ToLower() == x.ToLower()).ToList();
+                }
+                else
+                {
+                    Console.WriteLine($"No, My character is not {x}.");
+                    return playerBoard.Where(e => e.name.ToLower() != x.ToLower()).ToList();
+
+                }
+            }
+            else
+            {
+                questions();
+                return playerBoard;
+            }
         }
     }
 }
